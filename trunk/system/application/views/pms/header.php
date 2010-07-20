4452ae02b9f9f43671525eda09db357eb472e772
elimpg <elimpg@4f9d3e75-421f-6f28-c008-b44d82ee98a6>
2010-07-20 12:22:55 +0000

    views codigo modificado

---
+++ trunk/system/application/views/pms/header.php
@@ -0,0 +1,10 @@
+
+<p>
+<!-- <a href="">Calendario</a> |  -->
+<a href="<?php echo base_url().'rooms/view_rooms/'; ?>">Habitaciones</a> |
+<a href="<?php echo base_url().'reservations/view_pending_reservations/'; ?>">Reservaciones</a> |
+<!-- <a href="">Check - Ins</a> |
+<a href="">Check - Outs</a> | -->
+<a href="<?php echo base_url().'guests/view_guests/'; ?>">Clientes</a>
+</p>
+<br />
